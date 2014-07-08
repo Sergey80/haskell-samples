5d35eee6290aac2fd606e7b1dd651c4c8694ace9
Sergey80 <ses.box@gmail.com>
2014-07-07 23:56:04 -0400

    adde: some type_system stuff + couple of ghci commands

--- src/04_type_system.hs
+++ src/04_type_system.hs
@@ -3,16 +3,16 @@
 -- Interesting: Where C compiler will silently adjust/case type from int to float (for example),
 -- there Haskell compiler will raise a compilation error in a similar situation.
 
-f       ::  Double -> Double
+f       ::  Double -> Double        -- functtion that expects Double value and returns Double value
 f x     =   x + 1
 
-p1 = 1 :: Double         -- explicitly saying that literal is Double
+p1 = 1 :: Double                    -- explicitly saying that literal is Double
 p2 = 1 :: Integer
 
-result1 = f p1            -- 2.0
---result2 = f p2          -- this will not work out, since f expects Double not not Integer
+result1 = f p1                      -- 2.0
+--result2 = f p2                    -- this will not work out, since f expects Double not not Integer
 
 -- same result
---f (toInteger 1)          -- caseting to Integer expclicitly
+--f (toInteger 1)                   -- caseting to Integer expclicitly
 
-result3 = f 1              -- otherwise type inferring will create Double value
+result3 = f 1                       -- otherwise type inferring will create Double value

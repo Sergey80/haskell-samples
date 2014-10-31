7754c276b92aaf82369bf43c5ec1bac7d838de8e
Sergey80 <ses.box@gmail.com>
2014-10-31 16:57:58 -0400

    09 - conditions
    guarded equations

--- src/09_if_then.hs
+++ src/09_if_then.hs
@@ -1,6 +1,13 @@
--- #condition #if #else
+-- #condition #if #else #guarded equations
 a = 1
 result = if a == 1 then "one" else "two maybe"        -- one (do not forget about "then")
 
+-- another way to write conditions
+signum1 n | n < 0 = -1
+          | n == 0 = 0
+          | otherwise = 1
 
+a1 = signum1 0
+a2 = signum1 1
+a3 = signum1 (-1)
 

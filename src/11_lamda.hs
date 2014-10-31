ac2ff47f62643414937b368550b719d0a92ec1e9
Sergey80 <ses.box@gmail.com>
2014-10-31 17:20:21 -0400

    11 - lambda
    simple example

--- src/11_lamda.hs
+++ src/11_lamda.hs
@@ -8,4 +8,12 @@ add2  = \x -> (\y -> x + y)
 a = add1 2 2        -- 4
 b = add2 2 2        -- 4
 
+-- 2
+const1 :: a -> b -> a
+const1 x _ = x           -- whatever you pass as a second param - it will return x (firs paramteter)
+
+-- more naturally defined by lambda
+const2 :: a -> (b -> a)
+const2 x = \_ -> x
+
 

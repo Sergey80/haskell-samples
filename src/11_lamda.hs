ed883358cadd5d2998771d7dd042656c2f8edc3f
Sergey80 <ses.box@gmail.com>
2014-10-31 17:30:32 -0400

    11 - lambda
    simple example

--- src/11_lamda.hs
+++ src/11_lamda.hs
@@ -17,3 +17,15 @@ const2 :: a -> (b -> a)
 const2 x = \_ -> x
 
 
+-- if we want to name a function
+
+odds1 n = map f [0..n-1]         -- here we are defining a name "f"
+          where
+            f x = x*2 + 1
+
+odds2 n = map (\x -> x *2 +1) [0..n-1]      -- here we just are passing a lambda  - idiomatic haskell style
+
+r1 = odds1 5  -- [1,3,5,7,9]
+r2 = odds2 5  --
+
+
